05b9c4b3721e0a3f4e80d4b178070efdbdff6f0d
ahincho <ahincho@unsa.edu.pe>
2024-06-08 15:15:32 -0500

    Module configuration

--- freeCodeCamp/01-typescript-tutorial/src/main.ts
+++ freeCodeCamp/01-typescript-tutorial/src/main.ts
@@ -2,7 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
-import './02-tutorial.ts'
+import './09-tutorial.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
