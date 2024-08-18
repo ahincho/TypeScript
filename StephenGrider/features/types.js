869cf4ad1e532c4f203e5da8efc9f568feac2515
ahincho <ahincho@unsa.edu.pe>
2024-08-18 09:42:12 -0500

    Types

---
+++ StephenGrider/features/types.js
@@ -0,0 +1,13 @@
+var today = new Date();
+today.getMonth();
+var person = {
+    age: 20,
+};
+var Color = /** @class */ (function () {
+    function Color(name) {
+        this.name = name;
+    }
+    return Color;
+}());
+;
+var red = new Color("red");
