0a0ef3713173b658cf5f6dbee2fcf978ec8b73fa
ahincho <ahincho@unsa.edu.pe>
2024-08-18 11:13:34 -0500

    Tuples

---
+++ StephenGrider/features/tuples.js
@@ -0,0 +1,18 @@
+var drink = {
+    color: 'Brown',
+    carbonated: true,
+    sugar: 40
+};
+var pepsi = ['Brown', true, 40];
+// pepsi[0] = 40;
+var sprite = ['Clear', true, 40];
+var tea = ['Brown', false, 0];
+var drinks = [pepsi, sprite, tea];
+var carSpecs = [400, 3354];
+var carStats = {
+    horsePower: 400,
+    weight: 3354
+};
+console.log(drinks);
+console.log(carSpecs);
+console.log(carStats);
