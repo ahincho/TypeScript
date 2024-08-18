4058552f8ceba7ad2a615c03b458776894e3857c
ahincho <ahincho@unsa.edu.pe>
2024-08-18 18:01:31 -0500

    Classes

---
+++ StephenGrider/features/classes.ts
@@ -0,0 +1,24 @@
+class Vehicle {
+  constructor(public color: string) {}
+  protected honk(): void {
+    console.log('Beep');
+  }
+}
+const vehicle = new Vehicle('Orange');
+class Car extends Vehicle {´
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+  private drive(): void {
+    console.log('Vroom');
+  }
+  public startDrivingProcess(): void {
+    this.drive();
+  }
+  public startHonkProcess(): void {
+    this.honk();
+  }
+}
+const car = new Car(4, 'White');
+car.startDrivingProcess();
+car.startHonkProcess();
